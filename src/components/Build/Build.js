16458df815d7a9e4cec42dedf193188c24bfde4a
Dickson Eneh <dickseneh99@gmail>
2022-02-24 15:25:42 +0100

    props swap

--- src/components/Build/Build.js
+++ src/components/Build/Build.js
@@ -1,73 +1,101 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Build = ({picture, text, description, direction, input}) => {
+const Build = ({datee ,Cours, Pric, click, swap, move}) => {
   return (
-
 <Compos>
-<Composwraper direction ={direction}>
-<Composleft  src= {picture}/>
-
-<Composright>
-<Texthead>
-{text}
-</Texthead>
-<Textbody>
-{description}
-</Textbody>
-<Butts>
-{input}
-</Butts>
-</Composright>
-
-
-</Composwraper>
+  <Compswrap>
+   <Parent>
+    <Box>
+      <Midwrap swap = {swap}>
+    <Smallbox>
+   {datee}
+    </Smallbox>
+    <Course move = {move}>{Cours}</Course>
+    <Price move = {move}>{Pric}</Price>
+    </Midwrap>
+    <Butts>{click}</Butts>
+    </Box>
+   </Parent>
+  </Compswrap>
 </Compos>
+
   )
 }
 
 export default Build
 
-const Compos = styled.div`
-
-`
-const Composwraper= styled.div`
+const Compos= styled.div`
 display:flex;
 justify-content:center;
 align-items:center;
-flex-direction: ${({direction})=>direction}
+margin:30px;
+`
+const Compswrap=styled.div`
 
 `
-
-const Composleft = styled.img`
-width:400px;
-height:400px;
-border-radius:50%;
-objcet-fit:cover;
-margin:20px
+const Parent= styled.div`
+width:630px;
+background:black;
+height:180px;
+display:flex;
+justify-content:center;
+align-items:center;
+border-radius:10px
 `
-
-const Composright= styled.div`
-width:500px;
-margin:20px
-`
-const Texthead = styled.div`
-  font-weight:bold;
-  font-size:2rem;
-  margin-bottom:15px
-`
-
-const Textbody = styled.div`
-
+const Box= styled.div`
+width:600px;
+background:white;
+height:150px;
+border-radius:10px;
+display:flex;
+flex-direction:column;
+justify-content:center
 `
 const Butts = styled.button`
+background:purple;
 width:150px;
-padding:6px;
-background:orangered;
-color:white;
-font-size:20px;
-outline:none;
+padding:10px;
 border:none;
-border-radius:2px;
-margin-top:15px
+outline:none;
+color:white;
+border-radius:5px;
+margin-left:240px;
 `
+const Midwrap= styled.div`
+display:flex;
+justify-content:space-around;
+align-items:center;
+flex-direction:${({swap})=>swap}
+`
+const Smallbox= styled.button`
+width:60px;
+height:50px;
+background:black;
+border-radius:5px;
+color:white;
+font-size:10px;
+display:flex;
+justify-content:center;
+align-items:center;
+margin:5px;
+cursor:pointer;
+border:none;
+outline:none;
+    :hover{
+  background:silver;
+  border:black solid 1px;
+  color:black;
+
+}
+`
+const Course= styled.div`
+font-size:20px;
+margin-left:${({move})=>move}
+
+`
+const Price= styled.div`
+color:orange;
+margin-left:${({move})=>move}
+`
+
--- src/components/Homescreen/Home.js
+++ src/components/Homescreen/Home.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import styled from "styled-components"
-import Compos from '../Build/Build'
-import pic from '../images/parents.webp'
-import pic1 from '../images/teach.png'
+import Compos from "../Build/Build"
 
 
 
 
-const Home = () => {
+
+const Home = (  ) => {
+ 
+  const course = 'javascript'
+  const price = "$400"
+  const botts = "Add more"
   return (
-  
-    <Homeside>
-    <Compos picture ={pic} text = " Educators " description = "Donec euismod et lectus at dignissim. Morbi rhoncus, est quis porttitor gravida, ex dui ullamcorper tortor, sit amet porttitor ligula arcu eget nunc. Cras condimentum tristique lorem sed interdum. Praesent varius rutrum orci, quis ornare odio sagittis at. Nullam convallis bibendum metus, consequat rhoncus turpis viverra id. Nunc vitae " input ="
-    Learn More"/>
-    <Compos picture  = {pic1} text = "Teachers" description = "porta augue. Morbi vulputate ligula id dui vulputate, et rhoncus orci semper. Nulla facilisi. Fusce vestibulum dapibus ligula et vestibulum. Ut eu neque non massa tincidunt rutrum. Donec non tincidunt ipsum. Phasellus tincidunt sem iaculis, tincidunt eros sed, accumsan eros. Mauris vitae consectetur sapien, in scelerisque felis. Proin iaculis, sem consectetur aliquet sagittis, nisl eros tempor magna, in consectetur arcu diam ut nisi." direction ="row-reverse" input ="Learn More"/>
-
-    </Homeside>
+   <Body>
+<Compos datee = "November 12 2020" Cours = {course} Pric= {price} click = {botts}/>
+<Compos datee = "October 14 1999"Cours= "Algorithm" Pric="$334" click="Add cart" swap ="row-reverse" move = "35px"/>
+<Compos datee ="June 12 2022" Cours = "Reactjs" Pric = "$600" click="Carts" />
+   </Body>
   )
 }
 
 export default Home
 
-const Homeside = styled.div`
-height:100vh;
+const Body = styled.div`
 
 `
+
 
 
 
